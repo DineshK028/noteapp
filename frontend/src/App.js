4b0843a017eee3bac396b7d253528a0f6588bb6c
DineshK <163395592+DineshK028@users.noreply.github.com>
2025-06-05 21:31:54 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,7 +11,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://noteappbackend-iota.vercel.app/"
+          href="https://noteapp-frontend-nu.vercel.app/"
           target="_blank"
           rel="noopener noreferrer"
         >
